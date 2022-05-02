5869d190f9e7be8d23f13c2179e6f857d4773715
offsujin <offsujin@gmail.com>
2022-05-02 22:55:22 +0900

    Init Projects

---
+++ BackEnd/ewhaclub/src/test/java/com/gdscewha/ewhaclub/EwhaclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdscewha.ewhaclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwhaclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
