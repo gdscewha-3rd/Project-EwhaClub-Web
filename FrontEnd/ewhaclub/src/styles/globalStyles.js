7d0bee911a71884160eaf59359be1a2bd348d432
Hennization <shy6295@ewhain.net>
2022-05-03 01:04:09 +0900

    초기세팅

---
+++ FrontEnd/ewhaclub/src/styles/globalStyles.js
@@ -0,0 +1,33 @@
+// 전역적인 스타일 설정
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  #root, body, html {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    font-size: 62.5%;
+    font-family: Roboto;
+  }
+  * {
+    box-sizing: border-box;
+    ::-webkit-scrollbar {
+      width: 10px;
+    }
+    ::-webkit-scrollbar-thumb {
+      background-color: transparent;
+    }
+    &:hover::-webkit-scrollbar-thumb {
+      background-color: #bfbfbf;
+      border-radius: 18px;
+    }
+  }
+  button:hover {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
