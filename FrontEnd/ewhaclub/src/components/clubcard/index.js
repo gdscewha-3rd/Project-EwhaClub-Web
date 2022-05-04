dfc3e9861a06e131766feafc78c62b0452c04304
JangAyeon <ayeon3052@ewhain.net>
2022-05-04 18:40:27 +0900

    Comment : test

--- FrontEnd/ewhaclub/src/components/clubcard/index.js
+++ FrontEnd/ewhaclub/src/components/clubcard/index.js
@@ -3,7 +3,6 @@ import dummy from "../../db/data.json";
 
 const Clubcard = () => {
   console.log(dummy.data);
-  return <>카드</>; /*dummy.dat.map((data) => console.log(data));*/
-};
+  return <>카드</>; 
 
 export default Clubcard;
