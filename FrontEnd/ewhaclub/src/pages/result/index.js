84929e5f38b85c46bb6547044b24f85ed73cc0e7
JangAyeon <ayeon3052@ewhain.net>
2022-05-04 21:09:26 +0900

    Feature (Component) : clubcard

--- FrontEnd/ewhaclub/src/pages/result/index.js
+++ FrontEnd/ewhaclub/src/pages/result/index.js
@@ -1,26 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import { colors, fonts, BORDER_RADIUS, BOX_SHADOW } from "styles/styleObj";
 import Clubcard from "components/clubcard/index";
-
-const Title = styled.div`
-  font-family: MinSans-Medium;
-  font-size: ${fonts.size.large};
-  font-weight: ${fonts.weight.bold};
-  color: ${colors.red.origin};
-  width: 500px;
-  height: 100px;
-  border-radius: ${BORDER_RADIUS};
-  box-shadow: ${BOX_SHADOW};
-`;
+import dummy from "../../db/data.json";
 
 const Result = () => {
   return (
     <>
-      <Title> Result 페이지</Title>
-      <Clubcard />
+      <div> Result 페이지</div>
+      <List>
+        {dummy.data.map((club) => (
+          <>{<Clubcard club={club} />}</>
+        ))}
+      </List>
     </>
   );
 };
 
 export default Result;
+
+const List = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  width: 100%;
+  row-gap: 15px;
+  column-gap: 15px;
+`;
