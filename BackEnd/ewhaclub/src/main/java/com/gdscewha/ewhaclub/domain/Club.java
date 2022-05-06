8f86b74b963f1096bffff68b81c9614c37b38f64
offsujin <offsujin@gmail.com>
2022-05-06 20:11:09 +0900

    Change snake_case to camelCase

--- BackEnd/ewhaclub/src/main/java/com/gdscewha/ewhaclub/domain/Club.java
+++ BackEnd/ewhaclub/src/main/java/com/gdscewha/ewhaclub/domain/Club.java
@@ -1,9 +1,6 @@
 package com.gdscewha.ewhaclub.domain;
 
-import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
@@ -25,48 +22,48 @@ public class Club {
     private String name;
 
     @Column(nullable = false, length = 45)
-    private String eng_name;
+    private String engName;
 
     @Column(nullable = false, length = 255)
-    private String short_description;
+    private String shortDescription;
 
     @Column(nullable = false, length = 255)
-    private String detail_description;
+    private String detailDescription;
 
     @Column(nullable = false, length = 255)
-    private String detail_activity;
+    private String detailActivity;
 
     @Column(nullable = false)
-    private LocalDateTime recruit_start;
+    private LocalDateTime recruitStart;
 
     @Column(nullable = false)
-    private LocalDateTime recruit_end;
+    private LocalDateTime recruitEnd;
 
     @Column(nullable = false, columnDefinition = "TINYINT(1)")
-    private boolean is_recruiting;
+    private boolean isRecruiting;
 
     @Column(nullable = false, length = 255)
-    private String recruit_requirement;
+    private String recruitRequirement;
 
     @Column(nullable = false, length = 255)
-    private String how_to_apply;
+    private String howToApply;
 
     @Column(nullable = false)
     @ColumnDefault("0")
-    private Long view_cnt;
+    private Long viewCnt;
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String main_img_url;
+    private String mainImgUrl;
 
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String poster_img_url;
+    private String posterImgUrl;
 
     @Column(columnDefinition = "TEXT")
-    private String insta_url;
+    private String instaUrl;
 
     @Column(columnDefinition = "TEXT")
-    private String facebook_url;
+    private String facebookUrl;
 
     @Column(columnDefinition = "TEXT")
-    private String youtube_url;
+    private String youtubeUrl;
 }
