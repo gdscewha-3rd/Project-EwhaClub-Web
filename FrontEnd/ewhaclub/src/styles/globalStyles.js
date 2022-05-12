b7a3bad9f50762a70613aeb33d45ba6c0031a2ae
Hennization <shy6295@ewhain.net>
2022-05-12 16:48:15 +0900

    refactor: font 파일 이동

--- FrontEnd/ewhaclub/src/styles/globalStyles.js
+++ FrontEnd/ewhaclub/src/styles/globalStyles.js
@@ -37,37 +37,6 @@ const GlobalStyle = createGlobalStyle`
   img{
     background-color: transparent;
   }
-
-  /*font*/
-      @font-face {
-        font-family: 'MinSans-Black';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/MinSans-Black.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-    }
-    @font-face {
-        font-family: 'MinSans-Bold';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/MinSans-Bold.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'MinSans-Medium';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/MinSans-Medium.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-    }
-
-    @font-face {
-        font-family: 'MinSans-Light';
-        src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/MinSans-Light.woff') format('woff');
-        font-weight: normal;
-        font-style: normal;
-  }
-
-  
-
 `;
 
 export default GlobalStyle;
