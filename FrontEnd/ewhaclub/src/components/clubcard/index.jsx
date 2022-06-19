b455f1f84c7516b3b51ae4a03233a3c6e4b55a46
JangAyeon <ayeon3052@ewhain.net>
2022-06-19 15:32:22 +0900

    Fix : recruit State CSS

--- FrontEnd/ewhaclub/src/components/clubcard/index.jsx
+++ FrontEnd/ewhaclub/src/components/clubcard/index.jsx
@@ -1,50 +1,56 @@
-import { colors, fonts, BORDER_RADIUS_1, BORDER_RADIUS_2, BOX_SHADOW } from "styles/styleObj";
+import {
+  colors,
+  fonts,
+  BORDER_RADIUS_1,
+  BORDER_RADIUS_2,
+  BOX_SHADOW,
+} from "styles/styleObj";
 import React from "react";
 
 import {
-    StyledRoot,
-    Thumbnail,
-    MainContainer,
-    SubContainer,
-    InfoWrap,
-    LikesWrap,
-    Title,
-    Description,
-    Category,
-    State,
-    LikesImg,
+  StyledRoot,
+  Thumbnail,
+  MainContainer,
+  SubContainer,
+  InfoWrap,
+  LikesWrap,
+  Title,
+  Description,
+  Category,
+  State,
+  LikesImg,
 } from "./style";
 import { HeartIcon } from "asset/icons";
 import { categories } from "constants/categories";
 import { state } from "constants/state";
 
 const Clubcard = ({ club }) => {
-    console.log(state[club.category].name);
-    return (
-        <StyledRoot>
-            <Thumbnail>
-                <img src={club.main_img_url} alt={club.name}></img>
-            </Thumbnail>
-            <MainContainer>
-                <Title>{club.name}</Title>
-                <Description>{club.short_description}</Description>
-            </MainContainer>
-            <SubContainer>
-                <InfoWrap>
-                    <Category color={categories[club.category].color}>
-                        {categories[club.category].name}
-                    </Category>
-                    <State color={state[club.is_recruiting].color}>
-                        {state[club.category].name}
-                    </State>
-                </InfoWrap>
-                <LikesWrap>
-                    <LikesImg src={HeartIcon} />
+  //console.log(state[club.category].name);
+  return (
+    <StyledRoot>
+      <Thumbnail>
+        <img src={club.main_img_url} alt={club.name}></img>
+      </Thumbnail>
+      <MainContainer>
+        <Title>{club.name}</Title>
+        <Description>{club.short_description}</Description>
+      </MainContainer>
+      <SubContainer>
+        <InfoWrap>
+          <Category color={categories[club.category].color}>
+            {categories[club.category].name}
+          </Category>
+          <State color={state[club.is_recruiting].color}>
+            {state[club.is_recruiting].name}
+          </State>
+        </InfoWrap>
+        <LikesWrap>
+          <LikesImg src={HeartIcon} />
 
-                    {/*<LikesCnt>65</LikesCnt>*/}
-                </LikesWrap>
-            </SubContainer>
-        </StyledRoot>
-    );
+          {/*<LikesCnt>65</LikesCnt>*/}
+        </LikesWrap>
+      </SubContainer>
+    </StyledRoot>
+  );
 };
 export default Clubcard;
