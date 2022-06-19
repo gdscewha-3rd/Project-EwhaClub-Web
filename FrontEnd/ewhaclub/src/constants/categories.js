69e36c17cb748d7a7adb8dc29274332c47fa6147
Hennization <shy6295@ewhain.net>
2022-06-19 14:48:38 +0900

    feat: 상수 색상 추가

--- FrontEnd/ewhaclub/src/constants/categories.js
+++ FrontEnd/ewhaclub/src/constants/categories.js
@@ -1,12 +1,14 @@
+import { colors } from "styles/styleObj";
+
 export const categories = [
-  { name: "모두", id: "" },
-  { name: "공연", id: 1 },
-  { name: "문화", id: 2 },
-  { name: "사회과학", id: 3 },
-  { name: "사회연대", id: 4 },
-  { name: "종교", id: 5 },
-  { name: "체육", id: 6 },
-  { name: "학술", id: 7 },
+    { name: "모두", id: "", color: "" },
+    { name: "공연", id: 1, color: `${colors.red.light}` },
+    { name: "문화", id: 2, color: `${colors.orange.origin}` },
+    { name: "사회과학", id: 3, color: `${colors.yellow.light}` },
+    { name: "사회연대", id: 4, color: `${colors.green.light}` },
+    { name: "종교", id: 5, color: `${colors.blue.light}` },
+    { name: "체육", id: 6, color: `${colors.purple.light}` },
+    { name: "학술", id: 7, color: `${colors.purple.origin}` },
 ];
 
-//카테고리 상수 느학
+//카테고리 상수
---
+++ FrontEnd/ewhaclub/src/constants/state.js
@@ -0,0 +1,21 @@
+import { colors } from "styles/styleObj";
+
+export const state_color = {
+    0: {
+        text: `${colors.black.grey}`,
+        color: `${colors.white.origin}`,
+        border: `${colors.black.light_grey_1}`,
+    },
+    1: {
+        text: `${colors.black.grey}`,
+        color: `${colors.black.light_grey_1}`,
+        border: `${colors.black.light_grey_1}`,
+    },
+    2: {
+        text: `${colors.white.origin}`,
+        color: `${colors.black.light_grey_4}`,
+        border: `${colors.black.light_grey_4}`,
+    },
+};
+
+// 모집 상태 상수 헤헷
